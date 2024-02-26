cc5f55896540d8d7bcc77ab2ce50be20e89de008
GABRIELLUIZMARTINS <gabriel.martins.luiz@gmail.com>
2024-02-26 17:11:47 -0300

    Add debug log

--- main/espnow_receiver_main.c
+++ main/espnow_receiver_main.c
@@ -8,12 +8,20 @@
 #include <string.h>
 
 // Estrutura para armazenar os dados recebidos
-typedef struct 
+// typedef struct 
+// {
+//     uint32_t valor1;
+//     uint32_t valor2;
+//     uint32_t valor3;
+// } Dados;
+
+typedef struct
 {
-    uint32_t valor1;
-    uint32_t valor2;
-    uint32_t valor3;
-} Dados;
+    uint32_t valor1;    /*!< Sinal value */
+    uint32_t valor2;    /*!< Baseline value */
+    uint32_t valor3;    /*!< Threshold value */
+    uint8_t valor4;     /*!< Product type */
+} Data_t;
 
 static const char *TAG = "espnow_recv";
 
@@ -30,17 +38,17 @@ static void wifi_init()
 void on_data_received(const uint8_t *mac_addr, const uint8_t *data, int len) 
 {
     // Verificar se os dados recebidos têm o tamanho correto
-    if (len == sizeof(Dados)) {
+    if (len == sizeof(Data_t)) {
         // Converter os dados recebidos para a estrutura Dados
-        Dados *dados_recebidos = (Dados *)data;
+        Data_t *dados_recebidos = (Data_t *)data;
 
         // Exibir os dados recebidos
         // ESP_LOGI(TAG, "Dados recebidos de %02x:%02x:%02x:%02x:%02x:%02x",
         //          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-        ESP_LOGI(TAG, "(%d;%d;%d)",
-                 dados_recebidos->valor1, dados_recebidos->valor2, dados_recebidos->valor3);
+        ESP_LOGI(TAG, "(%d;%d;%d;%d)",
+                 dados_recebidos->valor1, dados_recebidos->valor2, dados_recebidos->valor3,dados_recebidos->valor4);
     } else {
-        ESP_LOGW(TAG, "Tamanho de dados incorreto recebido: %d, esperado: %d", len, sizeof(Dados));
+        ESP_LOGW(TAG, "Tamanho de dados incorreto recebido: %d, esperado: %d", len, sizeof(Data_t));
     }
     vTaskDelay(pdMS_TO_TICKS(1/1000)); // Delay 10 milisegundos
 
