1fbaaf8eae84d477bf284146bab45c01a7c72cba
GABRIELLUIZMARTINS <gabriel.martins.luiz@gmail.com>
2024-02-27 08:19:57 -0300

    Add debug and update main

--- main/espnow_receiver_main.c
+++ main/espnow_receiver_main.c
@@ -8,21 +8,22 @@
 #include <string.h>
 
 // Estrutura para armazenar os dados recebidos
-// typedef struct 
-// {
-//     uint32_t valor1;
-//     uint32_t valor2;
-//     uint32_t valor3;
-// } Dados;
-
-typedef struct
+typedef struct 
 {
-    uint32_t valor1;    /*!< Sinal value */
-    uint32_t valor2;    /*!< Baseline value */
-    uint32_t valor3;    /*!< Threshold value */
-    uint8_t valor4;     /*!< Product type */
-} Data_t;
+    uint32_t valor1;
+    uint32_t valor2;
+    uint32_t valor3;
+    uint8_t valor4;
 
+} Dados_with_product;
+
+// Estrutura para armazenar os dados recebidos
+typedef struct 
+{
+    uint32_t valor1;
+    uint32_t valor2;
+    uint32_t valor3;
+} Dados;
 static const char *TAG = "espnow_recv";
 
 // Função de inicialização do WiFi
@@ -38,17 +39,27 @@ static void wifi_init()
 void on_data_received(const uint8_t *mac_addr, const uint8_t *data, int len) 
 {
     // Verificar se os dados recebidos têm o tamanho correto
-    if (len == sizeof(Data_t)) {
+    if (len == sizeof(Dados) ) 
+    {
         // Converter os dados recebidos para a estrutura Dados
-        Data_t *dados_recebidos = (Data_t *)data;
-
-        // Exibir os dados recebidos
-        // ESP_LOGI(TAG, "Dados recebidos de %02x:%02x:%02x:%02x:%02x:%02x",
-        //          mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+        Dados *dados_recebidos_1 = (Dados *)data;     
+        
+            ESP_LOGI(TAG, "(%d;%d;%d)",
+                            dados_recebidos_1->valor1, 
+                            dados_recebidos_1->valor2, 
+                            dados_recebidos_1->valor3);
+        
+    }
+    else if(len == sizeof(Dados_with_product))
+    {
+        Dados_with_product *dados_recebidos = (Dados_with_product *)data;   
         ESP_LOGI(TAG, "(%d;%d;%d;%d)",
-                 dados_recebidos->valor1, dados_recebidos->valor2, dados_recebidos->valor3,dados_recebidos->valor4);
+                dados_recebidos->valor1, 
+                dados_recebidos->valor2, 
+                dados_recebidos->valor3,
+                dados_recebidos->valor4);
     } else {
-        ESP_LOGW(TAG, "Tamanho de dados incorreto recebido: %d, esperado: %d", len, sizeof(Data_t));
+        ESP_LOGW(TAG, "Tamanho de dados incorreto recebido: %d, esperado: %d", len, sizeof(Dados));
     }
     vTaskDelay(pdMS_TO_TICKS(1/1000)); // Delay 10 milisegundos
 
